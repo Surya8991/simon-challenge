ba468fb23d846631c349fc6dac07c01c839f9afd
D3v1L8148 <85568108+D3v1L8148@users.noreply.github.com>
2022-02-17 11:14:32 +0530

    added sounds

---
+++ game.js
@@ -0,0 +1,22 @@
+// to create colors
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// to store game pattern
+var gamePattern = [];
+
+//To create a sequence from 1 to 3
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  randomChosenColour = buttonColours[randomNumber];
+
+  // to add the random color in game pattern
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  // to add audio to the chosen color
+  var audio = new Audio("sounds/" + randomChosenColour + "mp3");
+  audio.play();
+}
